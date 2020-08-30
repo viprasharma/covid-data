1924f5045cd3a9a98684c16d0dab943805338b48
viprasharma <54271021+viprasharma@users.noreply.github.com>
2020-08-30 16:29:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var playerCount;
+var allPlayers;
+var distance = 0;
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  canvas = createCanvas(400,400);
+  database = firebase.database();
+  game = new Game();
+  game.getState();
+  game.start();
+}
+
+
+function draw(){
+  
+  if(mousePressedOver(show)){
+    clear();
+    game.play();
+  }
+}
